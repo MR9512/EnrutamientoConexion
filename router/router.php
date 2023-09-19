1633a507a5ffd4f110b6dc9403e41631417b9df4
Marco Rodriguez <mromero.10@hotmail.com>
2023-09-18 18:08:43 -0600

    MARR: Creación de Error 404

--- router/router.php
+++ router/router.php
@@ -1,34 +1,64 @@
 <?php
-class Router {
-    //Almacenaran el nombre del controlador y el método que se ejecutarán en función de la URL solicitada
+//Encapsula la lógica de enrutamiento
+class router{
+    //Almacenaran el nombre del controlador y el método que se ejecutarán en función de la URL solicitada.
     private $controller;
     private $method;
-     //Se llama automáticamente cuando se crea una instancia de la clase router para llamar a un metodo
-    public function __construct() {
-        //Llamamos automáticamente a la función matchRoute()
+      //Se llama automáticamente cuando se crea una instancia de la clase router para llamar a un metodo
+      public function __construct(){
         $this->matchRoute();
-    }
-    public function matchRoute() {
-        //Dividimos la URL en fragmentos usando la constante URL (asumiendo que está definida)
-        $url = explode("/", URL);
-        //El segundo fragmento de la URL se usa como el nombre del controlador
-        $this->controller = $url[1];
-        //Dividimos el tercer fragmento de la URL para eliminar cualquier cadena de consulta
-        $metodo = explode("?", $url[2]);
-        //El primer fragmento del resultado es el nombre del método
-        $this->method = $metodo[0];
-        //Agregamos "Controller" al nombre del controlador
-        $this->controller = $this->controller . "Controller";
-        //Requerimos el archivo del controlador desde la carpeta "Controllers/"
-        require_once("Controllers/" . $this->controller . ".php");
-    }
-     //Crea una instancia del controlador determinado y ejecuta el método correspondiente basado en la información del método matchRoute
-    public function run() {
-        //Creamos una instancia del controlador utilizando el nombre almacenado en $this->controller
+      }
+      //Analiza la URL para extraer el nombre del controlador y el método a partir de la URL solicitada
+      public function matchRoute(){
+          //Divide la URL en partes utilizando '/' como separador
+          $url = explode("/", URL);
+          //El primer segmento después del dominio generalmente representa el controlador
+          $this->controller = $url[1];
+          //Divide el segundo segmento en caso de que contenga parámetros de consulta (por ejemplo, ?parametro=valor)
+          $metodo = explode("?", $url[2]);
+          //Verifica si no hay parámetros de consulta en la URL
+          if ($url[2] == "") {
+              // Si no hay parámetros de consulta, usa "dashboard" como el método por defecto
+              $this->method = "dashboard";
+          } else {
+              //Si hay parámetros de consulta, utiliza el primer segmento como el nombre del método
+              $this->method = $metodo[0];
+          }
+          //Forma el nombre completo del controlador añadiendo "Controller" al final
+          $this->controller = $this->controller . "Controller";
+           // Verifica si el archivo del controlador existe
+        if (file_exists("Controllers/" . $this->controller . ".php")) {
+            //Si existe, incluye el archivo del controlador correspondiente
+            require_once "Controllers/" . $this->controller . ".php";
+        } else {
+            // Si el controlador no existe, muestra una página de error 404
+            $this->show404Error();
+        }
+      }
+      //Crea una instancia del controlador determinado y ejecuta el método correspondiente basado en la información del método matchRoute
+      public function run(){
+        //Crea una instancia del controlador correspondiente
         $controller = new $this->controller();
-        //Llamamos al método del controlador especificado en $this->method
-        $metodo = $this->method;
-        $controller->$metodo();
+        // Verifica si el método existe en el controlador
+        if (method_exists($controller, $this->method)) {
+            //Obtiene el nombre del método a ejecutar desde la propiedad $this->method y lo guarda en una variable
+            $method = $this->method;
+            //Llama al método del controlador con el nombre obtenido
+            $controller->$method();
+        } else {
+            // Si el método no existe, muestra una página de error 404
+            $this->show404Error();
+        }
     }
+
+    private function show404Error() {
+        // Muestra un mensaje de error 404 en caso de no existir la ruta seleccionada
+        echo "Error 404 - Página no encontrada";
+        exit;
+    }
+    
 }
+
+/*Se encarga de manejar el enrutamiento de solicitudes y controlar cuál controlador y método (acción) 
+deben ejecutarse en función de la URL solicitada */
 ?>
